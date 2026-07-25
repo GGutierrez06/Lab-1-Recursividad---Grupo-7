6df40f18624b9ae01cf191fe6987fb141d41696e
David Suazo <dsuazo@gmail.com>
2026-07-25 09:06:43 -0600

    revision si es palindromo y posicio ticket

---
+++ lab1progra2/src/lab1progra2/PalindromoAir.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1progra2;
+
+/**
+ *
+ * @author David Suazo Palao
+ */
+public class PalindromoAir {
+   private Ticket[] posicion;
+    private static final double precio=100;
+    private static final double descuento=0.20;
+    
+    public PalindromoAir(){
+        this.posicion=new Ticket[30];
+    }
+    public Ticket[] getPosicion(){
+        return posicion;
+    }
+    
+    public int firstAvailable (int index){
+        if (index >= posicion.length){
+            return -1;
+        }
+        if (posicion[index] == null){
+            return index;
+        }
+        return firstAvailable (index+1);
+    }
+    public int searchPassenger(String name, int index){
+        if (index >= posicion.length){
+            return -1;
+        }
+        if (posicion[index] != null && posicion[index].getname()){
+            return index;
+        }
+        return searchPassenger (name, index + 1);
+    }
+    private boolean revisarpali( String palabra, int izq, int der){
+        if (izq>= der){
+            return true;
+        }
+        if (palabra.charAt(izq) != palabra.charAt(der)){
+            return false;
+        }
+        return revisarpali(palabra, izq +1, der -1);
+    }
+    public boolean isPalindromo(String nombre){
+        if (nombre==null || nombre.isEmpty()){
+            return false;
+        }
+        String palabra=nombre.toLowerCase();
+        return revisarpali (palabra, 0, palabra.length()-1);
+    }
+    public String printPassengers( int index){
+        if (index >=posicion.length){
+        return "";
+    }
+    String posactual= "";
+    if (posicion[index]!=null){
+        posactual =" Asiento asignado es: "+ (index+1) + posicion[index];
+    }
+    return posactual +printPassengers (index +1);
+    
+    }
+}
