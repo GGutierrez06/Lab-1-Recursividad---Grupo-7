1cf7ab4be3a6db20a02b2a296e14b4ec8cc2d701
Fercho <fernandoaltamirano.vdc@gmail.com>
2026-07-25 09:32:03 -0600

    Forzando la subida de clase pantalla

---
+++ Lab-1-Recursividad---Grupo-7/src/lab/pkg1/recursividad/grupo/pkg7/Pantalla.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab.pkg1.recursividad.grupo.pkg7;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+/**
+ *
+ * @author User
+ */
+public class Pantalla extends JFrame {
+
+    private JButton[] botonesAsientos = new JButton[30];
+    private JTextField txtNombrePasajero;
+    private JTextArea areaConsola;
+
+}
