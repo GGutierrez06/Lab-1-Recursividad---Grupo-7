11eb829256f386ce5577cd8e307183e5a7f6daab
Rafa <jose.rperezg07@gmail.com>
2026-07-25 10:38:48 -0600

    correcciones

--- lab1progra2/src/lab1progra2/Ticket.java
+++ lab1progra2/src/lab1progra2/Ticket.java
@@ -1,26 +1,26 @@
 package lab1progra2;
 
 public class Ticket {
-    private String nombre;
+    private String name;
     private double finalAmount;
     public double montoOriginal;
     public double descuento;
     public boolean esPalin;
 
-    public Ticket(String nombre, double finalAmount, double montoOriginal, double descuento, boolean esPalin){
-        this.nombre=nombre;
+    public Ticket(String name, double finalAmount, double montoOriginal, double descuento, boolean esPalin){
+        this.name=name;
         this.finalAmount = finalAmount;
         this.montoOriginal=montoOriginal;
         this.descuento=descuento;
         this.esPalin=esPalin;
     }
 
-    public String getNombre() {
-        return nombre;
+    public String getName() {
+        return name;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public double getFinalAmount() {
@@ -51,7 +51,7 @@ public class Ticket {
 
     public void print(){
         System.out.println("Datos del Cliente");
-        System.out.println("Nombre: " + this.getNombre());
+        System.out.println("Nombre: " + this.getName());
         System.out.println("Monto Original: " + this.getMontoOriginal());
         System.out.println("Total a pagar: " + this.getFinalAmount());
         if(this.esPalin){
