20702e35836a4a7fd4b73440c3430c63a1d204db
Fercho <fernandoaltamirano.vdc@gmail.com>
2026-07-25 09:26:25 -0600

    creacion de clase Pantalla y botones

---
+++ Lab-1-Recursividad---Grupo-7/src/lab/pkg1/recursividad/grupo/pkg7/Lab1RecursividadGrupo7.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab.pkg1.recursividad.grupo.pkg7;
+
+/**
+ *
+ * @author User
+ */
+public class Lab1RecursividadGrupo7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
